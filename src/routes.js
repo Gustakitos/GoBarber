28b4c36bf361c95f3d39168205ef7b71285e0645
Gustakitos <luisgustavolg1033@gmail.com>
2019-07-03 15:30:05 -0300

    api database configured with model

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
-import { Router } from 'express'
+import { Router } from 'express';
+import User from './app/models/User';
 
-const routes = new Router()
+const routes = new Router();
 
-routes.get('/', (req, res) => {
-    return res.json({ message: 'Hello World' })
-})
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Gustavo',
+    email: 'gustavo@example.com',
+    password_hash: '123456',
+  });
 
-export default routes
+  return res.json(user);
+});
+
+export default routes;
