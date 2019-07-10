6aed5c615db78c9948bfe12a56dc0906d05f38ec
Gustakitos <luisgustavolg1033@gmail.com>
2019-07-10 18:07:12 -0300

    user can cancel appointment and receive email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'b865cc4e3419b0',
+    pass: '8247fb456b9cc5',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
