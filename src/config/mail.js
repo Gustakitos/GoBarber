3bc5d06f327ff3116c73e30fa5a6cb2fe847dfdf
Gustakitos <luisgustavolg1033@gmail.com>
2019-07-12 14:58:41 -0300

    error handling and production ready

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: '2525',
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: 'b865cc4e3419b0',
-    pass: '8247fb456b9cc5',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
